0fc944b4cf0507dbc67b9e208b149a0b0a9bd67d
Melissa Mercado <melissa.a.merc@gmail.com>
2020-07-14 00:11:15 -0400

    added onclick, vars and comments

--- script.js
+++ script.js
@@ -1,39 +1,78 @@
-//DEPENDENCIES================================================
-//DOM Elements
-//Initial DATA
-var userSearch = "";
-var userCityBtn = "";
-var cityName = "";
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/forecast?q=" +
-  cityName +
-  "&appid=b4ca2617fda8551aa1b532379a0245d5";
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-  $("#searchinfo").text(result.list[0].weather[0].main);
-});
-//doc ready
+// DOM ELEMNTS
+// INITAL DATA
+// A user types in a city
+$("#city-info-button").on("click", function () {
+  // a user submits their search
+  console.log(city);
 
-$(document).ready(() => {
-  console.log("ready");
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=b4ca2617fda8551aa1b532379a0245d5";
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+   var iconCode = response.list[0].weather[0].icon;
+    var iconImage = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    console.log(iconCode);
+    $("#icon-image").attr("src", iconImage);
+  });
 });
 
-//FUNCTIONS====================================================
-//USER INPUT===================================================
-function citiesInterestedIn(){
-  localStorage.
+//vars for cities since this would be easier than pulling from api
+var apikey = b4ca2617fda8551aa1b532379a0245d
+var cities = {
+"Boston" : {cords: {latitude: 42.361145 , longitude: -71.057083} },
+"Maimi" : {cords: {latitude: 25.761681 , longitude: -80.191788} },
+"Bangkok" : {cords: {latitude: 13.756331 , longitude: 100.501762} },
+"Sydney" : {cords: {latitude: -33.868820, longitude: 151.209290} },
+"New York City" : {cords: {latitude: 40.7128 , longitude: 74.0060} },
 }
 
 
 
-//A user types a city
-$(searchBar)
+//display todays date somewhere
+var date = moment().format("M/D/YYYY");
+var now = moment().format("H");
+
+
+
+$(".search").on("click", function("M/D/YYYY")){
+  var date = moment();
+  var dateDisplay = date.format();
+  var newSpan = $("<span>"); //not sure what that is about
+newSpan.text(dateDisplay);
+newSpan.attr("class", "current-date");
+}
+
+$("#.cardheader")
+
+
+//button click woth city
+var cityName = $("#city").val();
+
+
+//add prevent default
+ 
 //A user submits their search
-//A users search history is saved
+
+
+//A users search history is saved local storage //append
+
 //then the currrent weather of that city shows up
+
+
+
+
+
+//cards display future weather
+//$("#")
+
+
+
+
 //they see the city name,
 //the date,
 //an icon representation of weather conditions,
@@ -50,6 +89,8 @@ $(searchBar)
 //an icon representation of weather conditions,
 //the temperature,
 //and the humidity
+
+
 //If the user searches for another city, then their past city search is saved
 //The user can click on past city searches and view the weather
 //Display Data
