5ac05f2fc233536ab7d626de212eeb75f47cd389
Melissa Mercado <melissa.a.merc@gmail.com>
2020-10-03 14:51:43 -0400

    cleaning up code

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ $(document).ready(); => {
 //   });
 
 //this variable was created to fulfill the
+var apiKey = "b4ca2617fda8551aa1b532379a0245d5";
 var searchBtn = $("#city-info-button");
 
 //button click, prevent default stops search from refreshing
@@ -50,128 +51,162 @@ searchBtn.on("click", function (event) {
   event.preventDefault();
   //City search function to grab weather data
   var citySearch = $("#citySearch").val();
-  console.log("City Search:", citySearch);
-  var apiKey = "b4ca2617fda8551aa1b532379a0245d5";
+
+  getWeather(citySearch);
+  getForecast(citySearch);
+});
+
+// 5 day forecast that is giving me issues
+function getWeather(citySearch) {
   var queryURLCurrent =
     " https://api.openweathermap.org/data/2.5/weather?q=" +
     citySearch +
     "&units=imperial&appid=" +
     apiKey;
-  console.log("queryURL:", queryURLCurrent);
-  //used imperial to get farenheight
-  //then the currrent weather of that city shows up
+
   $.ajax({
     url: queryURLCurrent,
     method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    // Pulls city name
-    var cityName = response.name;
-    $("#city-name").text(cityName);
-    console.log("cityName:", cityName);
-    // pulls city date
-    var date = moment();
-    var dateDisplay = date.format("dddd MMMM Do YYYY");
-    $("#date").text(dateDisplay);
-    console.log("Date Display:", dateDisplay);
-    // icon var that will display in card upon search
-    var iconCode = response.weather[0].icon;
-    var weatherIcon = "https://openweathermap.org/img/w/" + iconCode + ".png";
-    console.log(iconCode);
-    $("#icon-image").attr("src", weatherIcon);
-    // tempature var that pulls temp from api and fills it in card
-    var temperature = response.main.temp;
-    $("#temperature").text(temperature);
-    console.log("Temperature:", temperature);
-    // humidity pullled from api that will display along everything
-    var humidity = response.main.humidity;
-    $("#humidity").text(humidity);
-    console.log("Humidity", humidity);
-    // wind speed information pulled from api
-    var windSpeed = response.wind.speed;
-    $("#wind-speed").text(windSpeed);
-    console.log("Wind Speed:", windSpeed);
-    // uv index using long lat
-    var latitude = response.coord.lat;
-    var longitude = response.coord.lon;
-    var uvURL =
-      "https://api.openweathermap.org/data/2.5/uvi?appid=" +
-      apiKey +
-      "&lat=" +
-      latitude +
-      "&lon=" +
-      longitude;
-    console.log("uvURL:", uvURL);
-    //uv index stuff that I am not sure why isnt working
-    $.ajax({
-      url: uvURL,
-      method: "GET",
-    }).then(function (response) {
-      console.log("UV", response);
+  })
+    .then(function (response) {
+      // Pulls city name
+      $(".current-city").text(response.name);
+
+      // pulls city date
+      $("#date").text(moment().format("dddd MMMM Do YYYY"));
+
+      // icon var that will display in card upon search
+      $("#icon-image").attr(
+        "src",
+        "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+      );
+
+      // tempature var that pulls temp from api and fills it in card
+      $("#temperature").text(response.main.temp);
+
+      // humidity pullled from api that will display along everything
+      $("#humidity").text(response.main.humidity);
+
+      // wind speed information pulled from api
+      $("#wind-speed").text(response.wind.speed);
+
+      // uv index using long lat
+      getUVI(response.coord.lat, response.coord.lon);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function getUVI(latitude, longitude) {
+  var uvURL =
+    "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+    apiKey +
+    "&lat=" +
+    latitude +
+    "&lon=" +
+    longitude;
+  //uv index stuff that I am not sure why isnt working
+  $.ajax({
+    url: uvURL,
+    method: "GET",
+  })
+    .then(function (response) {
       var uvIndex = response.value;
       $("#uv-index").text(uvIndex);
+
       if (uvIndex < 2) {
         $(".index").attr("class", "low");
-        console.log("Dont worry, You're safe!");
-      }
-      if (uvIndex >= 2 && uvIndex <= 5) {
+      } else if (uvIndex < 5) {
         $(".index").attr("class", "moderate");
-        console.log("It's getting risky");
-      }
-      if (uvIndex > 5 && uvIndex <= 7) {
+      } else if (uvIndex < 7) {
         $(".index").attr("class", "high");
-        console.log("Uh oh!");
-      }
-      if (uvIndex > 7 && uvIndex <= 10) {
+      } else if (uvIndex < 10) {
         $(".index").attr("class", "very-high");
-        console.log("Yikes! You better stay inside!");
-      }
-      if (uvIndex > 10) {
+      } else {
         $(".index").attr("class", "extreme");
-        console.log("You will defintely ignite on fire");
       }
-      console.log("UV Index:", uvIndex);
+    })
+    .catch(function (err) {
+      console.log(err);
     });
-  });
-});
-// 5 day forecast that is giving me issues
+}
 
-var forecastURL =
-  "https://api.openweathermap.org/data/2.5/forecast?q=" +
-  citySearch +
-  "&appid=b4ca2617fda8551aa1b532379a0245d5" +
-  "&units=imperial";
-$.ajax({
-  url: forecastURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-  // day one
-  var firstDay = moment(response.list[0].dt_txt).format("dddd MMMM Do YYYY");
-  $("#one").text(firstDay);
-  console.log(firstDay);
-  $("#temperature-one").text(response.list[0].main.temp);
-  $("#humidity-one").text(response.list[0].main.humidity);
-  $("#icon-image").text(response.list[0].wind.speed);
-  //Day two
-  var secondDay = moment(response.list[0].dt_txt).format("dddd MMMM Do YYYY");
-  $("#temperature-two").text(response.list[8].main.temp);
-  $("#humidity-two").text(response.list[8].main.humidity);
-  $("#wind-speed-two").text(response.list[8].wind.speed);
-  $("#pressure-two").text(response.list[8].main.pressure);
+function getForecast(citySearch) {
+  var forecastURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    citySearch +
+    "&appid=" +
+    apiKey +
+    "&units=imperial";
+  $.ajax({
+    url: forecastURL,
+    method: "GET",
+  })
+    .then(function (response) {
+      console.log(response);
 
-  $("#temperature-three").text(response.list[16].main.temp);
-  $("#humidity-three").text(response.list[16].main.humidity);
-  $("#wind-speed-three").text(response.list[16].wind.speed);
-  $("#pressure-three").text(response.list[16].main.pressure);
+      var forecast_data = [];
+      for (var i = 0; i < response.list.length; i++) {
+        var time = response.list[i].dt_txt.split(" ")[1];
+        if (time === "12:00:00") {
+          forecast_data.push(response.list[i]);
+        }
+      }
 
-  $("#temperature-four").text(response.list[24].main.temp);
-  $("#humidity-four").text(response.list[24].main.humidity);
-  $("#wind-speed-four").text(response.list[24].wind.speed);
-  $("#pressure-four").text(response.list[24].main.pressure);
-
-  $("#temperature-five").text(response.list[32].main.temp);
-  $("#humidity-five").text(response.list[32].main.humidity);
-  $("#wind-speed-five").text(response.list[32].wind.speed);
-  $("#pressure-five").text(response.list[32].main.pressure);
-});
+      for (var i = 0; i < forecast_data.length; i++) {
+        console.log(forecast_data[i]);
+        var time = moment(forecast_data[i].dt_txt).format("dddd MMMM Do YYYY");
+        var forecast = $("[data-forecast='" + i + "']");
+        forecast.find(".forecast-date").text(time);
+      }
+      // day one
+      // var firstDay = moment(response.list[0].dt_txt).format(
+      //   "dddd MMMM Do YYYY"
+      // );
+      // $("#one").text(firstDay);
+      // $("#temperature-one").text(response.list[0].main.temp);
+      // $("#humidity-one").text(response.list[0].main.humidity);
+      // $("#wind-speed-one").text(response.list[8].wind.speed);
+      // $("#pressure-one").text(response.list[8].main.pressure);
+      // //Day two
+      // var secondDay = moment(response.list[0].dt_txt).format(
+      //   "dddd MMMM Do YYYY"
+      // );
+      // $("#two").text(secondDay);
+      // $("#temperature-two").text(response.list[8].main.temp);
+      // $("#humidity-two").text(response.list[8].main.humidity);
+      // $("#wind-speed-two").text(response.list[8].wind.speed);
+      // $("#pressure-two").text(response.list[8].main.pressure);
+      // //Day 3
+      // var thirdDay = moment(response.list[0].dt_txt).format(
+      //   "dddd MMMM Do YYYY"
+      // );
+      // $("#three").text(thirdDay);
+      // $("#temperature-three").text(response.list[16].main.temp);
+      // $("#humidity-three").text(response.list[16].main.humidity);
+      // $("#wind-speed-three").text(response.list[16].wind.speed);
+      // $("#pressure-three").text(response.list[16].main.pressure);
+      // //Day 4
+      // var fourthDay = moment(response.list[0].dt_txt).format(
+      //   "dddd MMMM Do YYYY"
+      // );
+      // $("#four").text(fourthDay);
+      // $("#temperature-four").text(response.list[24].main.temp);
+      // $("#humidity-four").text(response.list[24].main.humidity);
+      // $("#wind-speed-four").text(response.list[24].wind.speed);
+      // $("#pressure-four").text(response.list[24].main.pressure);
+      // //Day 5
+      // var fifthDay = moment(response.list[0].dt_txt).format(
+      //   "dddd MMMM Do YYYY"
+      // );
+      // $("#five").text(fifthDay);
+      // $("#temperature-five").text(response.list[32].main.temp);
+      // $("#humidity-five").text(response.list[32].main.humidity);
+      // $("#wind-speed-five").text(response.list[32].wind.speed);
+      // $("#pressure-five").text(response.list[32].main.pressure);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
